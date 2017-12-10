1acd6ef6bc4c0efe6c745a51caf6c4ab9fbe4c6f
Eduardo Souza <eduardofidera@gmail.com>
2017-12-10 21:13:12 -0200

    improved classes to stateless components

--- src/app.js
+++ src/app.js
@@ -56,49 +56,38 @@ class IndecisionApp extends React.Component
   }
 }
 
-class Header extends React.Component
-{
-  render(){
-    console.log(this.props);
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
 }
 
-class Action extends React.Component
-{
-  render(){
-    return (
-      <div>
-        <button 
-        onClick={this.props.handlePick}
-        disabled={!this.props.hasOptions}
-        
-        >
-        What should I do
-        </button>
-      </div>
-    )
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+      onClick={props.handlePick}
+      disabled={!props.hasOptions}
+      >
+      What should I do
+      </button>
+    </div>
+  );
 }
 
-class Options extends React.Component
-{
-  render(){
-    return (
-      <div>
-        <p>{this.props.options.length}</p>
-        <button onClick={this.props.handleDeleteOptions}>remove all</button>
-        {
-          this.props.options.map((option) => <Option key={option} optionText={option} />)
-        }
-      </div>
-    )
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <p>{props.options.length}</p>
+      <button onClick={props.handleDeleteOptions}>remove all</button>
+      {
+        props.options.map((option) => <Option key={option} optionText={option} />)
+      }
+    </div>
+  );
 }
 
 class AddOptions extends React.Component
@@ -135,15 +124,12 @@ class AddOptions extends React.Component
   }
 }
 
-class Option extends React.Component
-{
-  render(){
-    return (
-      <div>
-        <p>Option: {this.props.optionText}</p>
-      </div>
-    );
-  }
+const Option = (props) => {
+  return (
+    <div>
+      <p>Option: {props.optionText}</p>
+    </div>
+  );
 }
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
