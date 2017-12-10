47f445c5ab3295499899c2c5972fe4ff2e1eb975
Eduardo Souza <eduardofidera@gmail.com>
2017-12-10 17:30:02 -0200

    learning es6 classes

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -1,12 +1,58 @@
 class Person
 {
-    constructor(name = 'Anon'){
+    constructor(name = 'Anon', age = 0){
         this.name = name;
+        this.age = age;
+    }
+    getGreeting(){
+        return `Hi, I am ${ this.name }.`;
+    }
+    getDescription(){
+        return `${ this.name } is ${ this.age } years old`;
     }
 }
 
-const me = new Person('Eduardo Souza');
-console.log(me);
+class Student extends Person
+{
+    constructor(name, age, major){
+        super(name, age); // super is the parent class
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if (this.hasMajor()){
+            description += ` their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
 
-const other = new Person();
-console.log(other);
+class Traveler extends Person
+{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation()){
+            greeting += ` im visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Student('Eduardo Souza', 13, 'Computer Science');
+console.log(me.getDescription());
+
+const other = new Traveler();
+console.log(other.getGreeting());
+
+const traveler = new Traveler('Eduardo', 23, 'Philadelphia');
+console.log(traveler.getGreeting());
